c4e8e9aff07f5a4c1fb87b7bbf06bcf6238d2755
Yongveloper <aaa9015@naver.com>
2021-01-23 18:06:28 +0900

    노래 재생 코드 수정 1차

--- js/audio.js
+++ js/audio.js
@@ -53,35 +53,55 @@
 // playBtnContainer.addEventListener('click', handlePlayButtonClick);
 // audioNextBtn.addEventListener('click', handleNextButtonClick);
 
+const audioContainer = document.querySelector('#audioContainer');
 const playBtnContainer = document.querySelector('.js-onOffWrap');
 const playBtn = document.querySelector('.js-audioOnOff');
 const audioNextBtn = document.querySelector('.js-audioNextBtn');
+const source = document.querySelector('#audioSource');
+
+const MUSIC_COUNT = 5;
+
+let currentAudio = 1;
 
 function playAudio() {
-  const audioContainer = document.querySelector('#audioContainer');
-  var source = document.getElementById('audioSource');
-  source.src = 'audio/1.mp3';
-  audioContainer.load();
-
+  audioContainer.volume = 0.2;
+  audioContainer.loop = true;
   audioContainer.play();
   playBtn.innerText = '♬ Stop Music';
   audioNextBtn.classList.remove('invisiable');
 }
 
 function stopAudio() {
-  //   audioContainer.pause();
-  //   playBtn.innerText = '♬ Play Music';
-  //   audioNextBtn.classList.add('invisiable');
+  audioContainer.pause();
+  playBtn.innerText = '♬ Play Music';
+  audioNextBtn.classList.add('invisiable');
+}
+
+function loadAudio() {
+  source.src = `audio/${currentAudio}.mp3`;
+  audioContainer.load();
+  playAudio();
 }
 
 function handlePlayBtnClick() {
   const state = playBtn.innerText;
 
   if (state === '♬ Play Music') {
-    playAudio();
+    loadAudio();
   } else {
     stopAudio();
   }
 }
 
+function handleNextButtonClick() {
+  if (currentAudio < MUSIC_COUNT) {
+    currentAudio += 1;
+  } else {
+    currentAudio = 1;
+  }
+  audioContainer.pause();
+  loadAudio();
+}
+
 playBtnContainer.addEventListener('click', handlePlayBtnClick);
+audioNextBtn.addEventListener('click', handleNextButtonClick);
