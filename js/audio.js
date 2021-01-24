7c9a67df863ec0813e6ffc156dfda8bf8e689f2a
Yongveloper <aaa9015@naver.com>
2021-01-24 16:21:51 +0900

    HTML,CSS,audio.js: 음악 갯수 표시

--- js/audio.js
+++ js/audio.js
@@ -4,7 +4,7 @@ const audioContainer = document.querySelector('#audioContainer');
 const playBtnContainer = document.querySelector('.js-onOffWrap');
 const playBtn = document.querySelector('.js-audioOnOff');
 const audioNextBtn = document.querySelector('.js-audioNextBtn');
-const source = document.querySelector('#audioSource');
+const audioCountDisplay = document.querySelector('.js-audioNumDisplay');
 
 const MUSIC_COUNT = 5;
 
@@ -16,15 +16,18 @@ function playAudio() {
   audioContainer.play();
   playBtn.innerText = '♬ Stop Music';
   audioNextBtn.classList.remove('invisiable');
+  audioCountDisplay.classList.remove('invisiable');
 }
 
 function stopAudio() {
   audioContainer.pause();
   playBtn.innerText = '♬ Play Music';
   audioNextBtn.classList.add('invisiable');
+  audioCountDisplay.classList.add('invisiable');
 }
 
 function loadAudio() {
+  const source = document.querySelector('#audioSource');
   source.src = `audio/${currentAudio}.mp3`;
   audioContainer.load();
   playAudio();
@@ -41,11 +44,15 @@ function handlePlayBtnClick() {
 }
 
 function handleNextButtonClick() {
+  const audioNum = document.querySelector('.js-audioNum');
+
   if (currentAudio < MUSIC_COUNT) {
     currentAudio += 1;
   } else {
     currentAudio = 1;
   }
+
+  audioNum.innerText = currentAudio;
   audioContainer.pause();
   loadAudio();
 }
