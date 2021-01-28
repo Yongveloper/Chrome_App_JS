c1db256abc6c6875a4e89e71f0f8f92c584f23b6
Yongveloper <aaa9015@naver.com>
2021-01-29 00:31:26 +0900

    audio.js: audio timer 기능 추가

--- js/audio.js
+++ js/audio.js
@@ -5,14 +5,18 @@ const playBtnContainer = document.querySelector('.js-onOffWrap');
 const playBtn = document.querySelector('.js-audioOnOff');
 const audioNextBtn = document.querySelector('.js-audioNextBtn');
 const audioCountDisplay = document.querySelector('.js-audioNumDisplay');
+const audioTime = document.querySelector('.js-audioTime');
 
 const MUSIC_COUNT = 5;
 const PLAY_MUSIC = '♬ Play BGM';
 const STOP_MUSIC = '♬ Stop BGM';
 const AUDIO_VOLUME = 0.2;
 const INVISABLE = 'invisiable';
+const START_TIME = 0;
 
 let currentAudio = 1;
+let timerInterval;
+let time = START_TIME;
 
 async function playAudio() {
   playBtn.innerText = STOP_MUSIC;
@@ -21,16 +25,15 @@ async function playAudio() {
   audioContainer.volume = AUDIO_VOLUME;
   audioContainer.loop = false;
   await audioContainer.play();
-  var minutes = parseInt(audioContainer.duration / 60, 10);
-  var seconds = parseInt(audioContainer.duration % 60);
-  console.log(minutes, seconds);
+  timerInterval = setInterval(increaseAudioTimer, 1000);
 }
 
 function stopAudio() {
-  audioContainer.pause();
   playBtn.innerText = PLAY_MUSIC;
   audioNextBtn.classList.add(INVISABLE);
   audioCountDisplay.classList.add(INVISABLE);
+  audioContainer.pause();
+  resetAudioTimer();
 }
 
 function loadAudio() {
@@ -61,7 +64,22 @@ function loadNextAudio() {
 
   audioNum.innerText = currentAudio;
   audioContainer.pause();
+  resetAudioTimer();
   loadAudio();
+}
+
+function increaseAudioTimer() {
+  time++;
+  let mins = Math.floor((time % 3600) / 60);
+  let secs = (time % 3600) % 60;
+
+  audioTime.innerHTML = `${mins < 10 ? `0${mins}` : mins}:${secs < 10 ? `0${secs}` : secs}`;
+}
+
+function resetAudioTimer() {
+  clearInterval(timerInterval);
+  time = START_TIME;
+  audioTime.innerHTML = '00:00';
 }
 
 playBtnContainer.addEventListener('click', handlePlayBtnClick);
