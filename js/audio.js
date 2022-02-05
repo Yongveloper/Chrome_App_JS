ba9dc7ee915214822167a6c561f2e7386fa7f3ef
Yongveloper <aaa9015@naver.com>
2022-02-05 20:19:19 +0900

    audio.js 리팩토링 함수 위치, 함수 분할

--- js/audio.js
+++ js/audio.js
@@ -16,17 +16,15 @@ const START_TIME = 0;
 
 let isPlaying = false;
 let currentAudio = 1;
-let timerInterval;
+let timerInterval = null;
 let time = START_TIME;
 
-async function playAudio() {
-  playBtn.innerText = STOP_MUSIC;
-  audioNextBtn.classList.remove(INVISABLE);
-  audioCountDisplay.classList.remove(INVISABLE);
-  audioContainer.volume = AUDIO_VOLUME;
-  audioContainer.loop = false;
-  await audioContainer.play();
-  timerInterval = setInterval(increaseAudioTimer, 1000);
+function handlePlayBtnClick() {
+  if (isPlaying) {
+    stopAudio();
+  } else {
+    loadAudio();
+  }
 }
 
 function stopAudio() {
@@ -38,27 +36,23 @@ function stopAudio() {
   resetAudioTimer();
 }
 
-function loadAudio() {
-  isPlaying = true;
-  const source = document.querySelector('#audioSource');
-  source.src = `audio/${currentAudio}.mp3`;
-  audioContainer.load();
-  playAudio();
+function resetAudioTimer() {
+  clearInterval(timerInterval);
+  time = START_TIME;
+  displayAudioTime(0, 0);
 }
 
-function handlePlayBtnClick() {
-  if (isPlaying) {
-    stopAudio();
-  } else {
-    loadAudio();
-  }
+function displayAudioTime(mins, secs) {
+  audioTime.innerHTML = `${mins < 10 ? `0${mins}` : mins}:${
+    secs < 10 ? `0${secs}` : secs
+  }`;
 }
 
 function loadNextAudio() {
   const audioNum = document.querySelector('.js-audioNum');
 
   if (currentAudio < MUSIC_COUNT) {
-    currentAudio += 1;
+    currentAudio++;
   } else {
     currentAudio = 1;
   }
@@ -69,18 +63,35 @@ function loadNextAudio() {
   loadAudio();
 }
 
-function increaseAudioTimer() {
-  time++;
-  let mins = Math.floor((time % 3600) / 60);
-  let secs = (time % 3600) % 60;
-
-  audioTime.innerHTML = `${mins < 10 ? `0${mins}` : mins}:${secs < 10 ? `0${secs}` : secs}`;
+function loadAudio() {
+  const source = document.querySelector('#audioSource');
+  source.src = `audio/${currentAudio}.mp3`;
+  isPlaying = true;
+  audioContainer.load();
+  playAudio();
 }
 
-function resetAudioTimer() {
-  clearInterval(timerInterval);
-  time = START_TIME;
-  audioTime.innerHTML = '00:00';
+async function playAudio() {
+  playBtn.innerText = STOP_MUSIC;
+  audioNextBtn.classList.remove(INVISABLE);
+  audioCountDisplay.classList.remove(INVISABLE);
+  audioContainer.volume = AUDIO_VOLUME;
+  audioContainer.loop = false;
+  await audioContainer.play();
+  timerInterval = setInterval(increaseAudioTimer, 1000);
+}
+
+function increaseAudioTimer() {
+  time++;
+  const { mins, secs } = getPlayTime();
+  displayAudioTime(mins, secs);
+}
+
+function getPlayTime() {
+  const mins = Math.floor((time % 3600) / 60);
+  const secs = (time % 3600) % 60;
+
+  return { mins, secs };
 }
 
 playBtnContainer.addEventListener('click', handlePlayBtnClick);
